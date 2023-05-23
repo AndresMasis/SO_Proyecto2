24112cc186940934db3433afcc82de1c89995126
LeonardoFarina <65524519+LeonardoFarina@users.noreply.github.com>
2023-05-22 22:58:30 -0600

    Delete compilar.sh

--- compilar.sh
+++
@@ -1,6 +0,0 @@
-gcc -o Pinit Pinit.c -w -lncurses
-gcc -o Pwriter Pwriter.c -w -lncurses
-gcc -o Preader Preader.c -w -lncurses
-gcc -o PreaderEgo PreaderEgo.c -w -lncurses
-gcc -o Pspy Pspy.c -w -lncurses
-gcc -o Pinit Pinit.c -w -lncurses
